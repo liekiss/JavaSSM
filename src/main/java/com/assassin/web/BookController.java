e8012a7f272da2e5d3fc0b0014ec951f2aed5602
liekiss <289294189@qq.com>
2017-04-16 19:57:27 +0800

    finish

---
+++ src/main/java/com/assassin/web/BookController.java
@@ -0,0 +1,7 @@
+package com.assassin.web;
+
+/**
+ * Created by 28929_000 on 2017/4/16.
+ */
+public class BookController {
+}
---
+++ src/test/java/com/assassin/service/impl/BookServiceImplTest.java
@@ -0,0 +1,7 @@
+package com.assassin.service.impl;
+
+/**
+ * Created by 28929_000 on 2017/4/15.
+ */
+public class BookServiceImplTest {
+}
